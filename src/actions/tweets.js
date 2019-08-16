94aa27a9a81153781df22a20545bed3157752539
juliafealves <giT#8985>
2019-08-15 22:02:02 -0300

    New Tweet

--- src/actions/tweets.js
+++ src/actions/tweets.js
@@ -1,7 +1,31 @@
-import { saveLikeToggle } from "../utils/api";
+import { saveLikeToggle, saveTweet } from "../utils/api";
+import { hideLoading, showLoading } from "react-redux-loading";
 
 export const RECEIVE_TWEETS = "RECEIVE_TWEETS";
 export const TOGGLE_TWEET = "TOGGLE_TWEET";
+export const ADD_TWEET = "ADD_TWEET";
+
+export function addTweet(tweet) {
+  return {
+    type: ADD_TWEET,
+    tweet
+  };
+}
+
+export function handleAddTweet(text, replyingTo) {
+  return (dispatch, getState) => {
+    const { authedUser } = getState();
+    dispatch(showLoading());
+
+    return saveTweet({
+      text,
+      author: authedUser,
+      replyingTo
+    })
+      .then(tweet => dispatch(addTweet(tweet)))
+      .then(() => hideLoading());
+  };
+}
 
 export function receiveTweets(tweets) {
   return {

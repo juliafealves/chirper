63984a1119704776a455eebeafb69de9221874d5
juliafealves <juliafealves>
2019-08-17 21:50:47 -0300

    Tweet Page

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,10 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
+import LoadingBar from "react-redux-loading";
 import { handleInitialData } from "../actions/shared";
 import Dashboard from "./Dashboard";
 import NewTweet from "./NewTweet";
-import LoadingBar from "react-redux-loading";
+import TweetPage from "./TweetPage";
 
 class App extends Component {
   componentDidMount() {
@@ -14,7 +15,9 @@ class App extends Component {
     return (
       <div>
         <LoadingBar />
-        {this.props.loading ? null : <NewTweet />}
+        {this.props.loading ? null : (
+          <TweetPage match={{ params: { id: "8xf0y6ziyjabvozdd253nd" } }} />
+        )}
       </div>
     );
   }
